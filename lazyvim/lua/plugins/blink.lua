c68318f4efd6a6b0a34f85b776eac55e76d3b684
Constantijn Schepens <constantijnschepens@gmail.com>
2025-05-19 11:14:17 +0100

    Add hint

--- lazyvim/lua/plugins/blink.lua
+++ lazyvim/lua/plugins/blink.lua
@@ -5,6 +5,7 @@ return {
       local keymap = opts.keymap or {}
       -- Set <C-e> to {} to explicitly disable blink.cmp's internal mapping for it.
       -- This should prevent blink.cmp from setting it, allowing my global mapping to take over <Esc> to work.
+      -- Hint: if this breaks in the future, use ":verbose imap <C-e>" to find out where it's being set.
       keymap["<C-e>"] = {}
       opts.keymap = keymap
 
