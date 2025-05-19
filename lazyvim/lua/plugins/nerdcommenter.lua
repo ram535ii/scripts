6d9a38fc4874715c60ddd93f36ef5fea5ac04b45
Constantijn Schepens <constantijnschepens@gmail.com>
2025-05-19 10:07:52 +0100

    Add nerdcommenter

---
+++ lazyvim/lua/plugins/nerdcommenter.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "preservim/nerdcommenter",
+    opts = {
+      NERDSpaceDelims = 1, -- Add spaces after comment delimiters by default
+    },
+    config = function()
+      -- <leader>ci - to comment/uncomment multiline.
+    end,
+  },
+}
