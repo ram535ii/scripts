1f3aff487c9fa886035d0ed629239fa6a9aabdcd
Constantijn Schepens <constantijnschepens@gmail.com>
2025-05-30 10:13:16 +0100

    Add extras and update ge to ce

--- lazyvim/lua/plugins/lsp-config.lua
+++ lazyvim/lua/plugins/lsp-config.lua
@@ -3,7 +3,7 @@ local path = require("plenary.path")
 
 local servers = {
   "gopls",
-	"protols"
+  "protols",
 }
 
 for _, lsp in ipairs(servers) do
@@ -12,7 +12,7 @@ end
 
 -- Automatically open LSP-powered autocompletion.
 local setup_completion = function(client, bufnr)
-  if client:supports_method('textDocument/completion') then
+  if client:supports_method("textDocument/completion") then
     vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
   end
 end
@@ -25,10 +25,9 @@ local custom_lsp_attach = function(setup_callbacks)
   end
 end
 
-
 local function service_root_dir(bufnr, cb)
   local buffer_filepath = vim.api.nvim_buf_get_name(bufnr)
-	-- Treat anything containing these files as a root directory. This
+  -- Treat anything containing these files as a root directory. This
   -- prevents us ascending too far toward the root of the repository, which
   -- stops us from trying to ingest too much code.
   local root_markers = { "main.go", "README.md", "go.mod", "LICENSE" } -- Add more as needed
@@ -42,56 +41,55 @@ return {
     enabled = path:new(os.getenv("HOME") .. "/src/github.com/monzo/wearedev"):exists(),
     config = function()
       vim.lsp.config("gopls", {
-				-- TODO(ram535ii): do I need this in a LazyVim world?
-				-- on_attach = custom_lsp_attach{ -- by default, set up everything!
-					-- setup_completion
-				-- },
+        -- TODO(ram535ii): do I need this in a LazyVim world?
+        -- on_attach = custom_lsp_attach{ -- by default, set up everything!
+        -- setup_completion
+        -- },
 
         cmd = { "env", "GO111MODULE=off", "gopls", "-remote=auto" },
 
-				settings = {
-					gopls = {
-						analyses = {
-							-- These are the only analyzers that are disabled by default in
-							-- gopls.
-							nilness = true,
-							shadow = true,
-							unusedparams = true,
-							unusedwrite = true,
-						},
-						buildFlags = { "-tags=integration" }, -- for LSP support in integration tests
-						staticcheck = true,
-						expandWorkspaceToModule = false,
-						-- Organize imports. Groups imports by Monzo versus not-Monzo.
-						-- Equivalent to local flag with goimports
-						["local"] = "github.com/monzo/wearedev",
-					},
-				},
-
+        settings = {
+          gopls = {
+            analyses = {
+              -- These are the only analyzers that are disabled by default in
+              -- gopls.
+              nilness = true,
+              shadow = true,
+              unusedparams = true,
+              unusedwrite = true,
+            },
+            buildFlags = { "-tags=integration" }, -- for LSP support in integration tests
+            staticcheck = true,
+            expandWorkspaceToModule = false,
+            -- Organize imports. Groups imports by Monzo versus not-Monzo.
+            -- Equivalent to local flag with goimports
+            ["local"] = "github.com/monzo/wearedev",
+          },
+        },
 
         root_dir = service_root_dir,
         filetypes = { "go", "yml" },
 
-				-- Never use wearedev as a root path. It'll grind your machine to a halt.
-				ignoredRootPaths = { "$HOME/src/github.com/monzo/wearedev/" },
+        -- Never use wearedev as a root path. It'll grind your machine to a halt.
+        ignoredRootPaths = { "$HOME/src/github.com/monzo/wearedev/" },
 
-				-- Collect less information about packages without open files.
-				memoryMode = "DegradeClosed",
+        -- Collect less information about packages without open files.
+        memoryMode = "DegradeClosed",
 
-				flags = {
-					-- gopls is a particularly slow language server, especially in wearedev.
-					-- Debounce text changes so that we don't send loads of updates.
-					debounce_text_changes = 500,
-				},
+        flags = {
+          -- gopls is a particularly slow language server, especially in wearedev.
+          -- Debounce text changes so that we don't send loads of updates.
+          debounce_text_changes = 500,
+        },
 
-				init_options = {
-					codelenses = {
-						generate = true,
-						gc_details = true,
-						test = true,
-							tidy = true,
-					},
-				},
+        init_options = {
+          codelenses = {
+            generate = true,
+            gc_details = true,
+            test = true,
+            tidy = true,
+          },
+        },
       })
 
       vim.lsp.config("protols", {
--- lazyvim/lua/plugins/vim-go.lua
+++ lazyvim/lua/plugins/vim-go.lua
@@ -5,7 +5,18 @@ return {
     config = function()
       vim.g.go_gopls_enabled = false -- or false to disable
       vim.g.go_def_mapping_enabled = false -- stops vim-go taking over <C-t> for tagstack jumps
-      vim.keymap.set("n", "<leader>ge", "<cmd>GoIfErr<CR>", { desc = "Go if error" })
+
+      -- Nice syntax highlighting
+      vim.g.go_highlight_types = true
+      vim.g.go_highlight_fields = true
+      vim.g.go_highlight_functions = true
+      vim.g.go_highlight_function_calls = true
+      vim.g.go_highlight_methods = true
+      vim.g.go_highlight_operators = true
+      vim.g.go_highlight_extra_types = true
+      vim.g.go_highlight_format_strings = true
+
+      vim.keymap.set("n", "<leader>ce", "<cmd>GoIfErr<CR>", { desc = "Go if error" })
       vim.keymap.set("n", "<leader>tf", "<cmd>GoAlternate<CR>", { desc = "Go to tests for file" })
       vim.keymap.set("n", "<leader>tc", "<cmd>GoCoverageToggle<CR>", { desc = "Test coverage toggle" })
     end,
@@ -17,7 +28,7 @@ return {
     config = function()
       vim.keymap.set(
         "n",
-        "<leader>gp",
+        "<leader>cp", -- code+protobuf
         require("protodef").protodef,
         { noremap = true, silent = true, desc = "ProtoDefinition" }
       )
