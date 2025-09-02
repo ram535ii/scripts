ad4422f358b106084205f68c99fa89f323e97cb6
Constantijn Schepens <constantijnschepens@gmail.com>
2025-09-02 16:51:27 +0100

    Disable animate

---
+++ lazyvim/lua/plugins/animate.lua
@@ -0,0 +1,5 @@
+return {
+  -- Disable mini.animate, it makes going up and down lines jump a lot when
+  -- doing so quickly (especially when holding down j/k).
+  { "echasnovski/mini.animate", enabled = false },
+}
