cfd33f4d26c8505db747c79baa8b6005ebf505c2
Constantijn Schepens <constantijnschepens@gmail.com>
2025-12-22 10:24:40 +0100

    Try a new approach to gitsign peacefulness without disabling entirely.

--- lazyvim/lua/plugins/gitsigns.lua
+++ lazyvim/lua/plugins/gitsigns.lua
@@ -5,7 +5,7 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    enabled = false,
+    enabled = true,
     opts = {
       --Disable inline blame text (avoids running `git blame` on every cursor move.
       current_line_blame = false,
@@ -17,11 +17,20 @@ return {
       update_debounce = 1000,
 
       --Control how Gitsigns watches the .git directory.
-      watch_gitdir = {
-        enable = true, -- Keep watching for repo changes
-        interval = 10000, -- Check every 10s instead of constantly
-        follow_files = true, -- Keep tracking signs if files are renamed/moved
-      },
+      --watch_gitdir = {
+      --enable = true, -- Keep watching for repo changes
+      --interval = 10000, -- Check every 10s instead of constantly
+      --follow_files = true, -- Keep tracking signs if files are renamed/moved
+      --},
+      -- Don't monitor .git in the background at all. If a buffer that is
+      -- visible is changed in the background, I need to manually refresh to see
+      -- the signs update.
+      watch_gitdir = { enable = false },
+    },
+    keys = {
+      -- "Experimental" commands.
+      { "<leader>gxu", "<cmd>lua require('gitsigns').refresh()<cr>", desc = "Gitsigns refresh" },
+      { "<leader>gxb", "<cmd>Gitsigns blame_line<cr>", desc = "Blame line" },
     },
   },
 }
